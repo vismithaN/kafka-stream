ad9b0c239826042646bdf283b6e4f2190c6a9a87
Vismitha N <vismitha944@gmail.com>
2024-11-14 15:54:50 -0500

    testing1

--- stream-processing/DataProducer/src/main/java/DataProducerRunner.java
+++ stream-processing/DataProducer/src/main/java/DataProducerRunner.java
@@ -28,7 +28,7 @@ public class DataProducerRunner {
         props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432");
 
         KafkaProducer<String,String> producer = new KafkaProducer<>(props);
-        Path traceFilePath = Paths.get("stream-processing", "DataProducer", "tracefile");
+        Path traceFilePath = Paths.get("tracefile");
 
         // Convert the Path to an absolute path
         String absolutePath = traceFilePath.toAbsolutePath().toString();
