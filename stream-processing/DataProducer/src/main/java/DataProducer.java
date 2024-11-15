c10f71561e45c73f39526bf55caf6ce8bde53a2f
Vismitha N <vismitha944@gmail.com>
2024-11-15 14:22:01 -0500

    Added tests

--- stream-processing/DataProducer/src/main/java/DataProducer.java
+++ stream-processing/DataProducer/src/main/java/DataProducer.java
@@ -1,4 +1,13 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 public class DataProducer {
     private Producer<String, String> producer;
@@ -10,17 +19,34 @@ public class DataProducer {
     }
 
     /**
-      Task 1:
-        In Task 1, you need to read the content in the tracefile we give to you, 
-        create two streams, and feed the messages in the tracefile to different 
-        streams based on the value of "type" field in the JSON string.
+     Task 1:
+     In Task 1, you need to read the content in the tracefile we give to you,
+     create two streams, and feed the messages in the tracefile to different
+     streams based on the value of "type" field in the JSON string.
 
-        Please note that you're working on an ec2 instance, but the streams should
-        be sent to your samza cluster. Make sure you can consume the topics on the
-        master node of your samza cluster before you make a submission.
-    */
+     Please note that you're working on an ec2 instance, but the streams should
+     be sent to your samza cluster. Make sure you can consume the topics on the
+     master node of your samza cluster before you make a submission.
+     */
     public void sendData() {
+        try(BufferedReader br = new BufferedReader(new FileReader(traceFileName))) {
+            String log;
+            while((log = br.readLine()) != null) {
+                JsonParser parser = new JsonParser();
+                JsonElement jsonElement = parser.parse(log);
+                JsonObject json = jsonElement.getAsJsonObject();
 
+                String type = json.get("type").getAsString();
+                int blockId = json.get("blockId").getAsInt();
+                String topic = type.equals("DRIVER_LOCATION") ? "driver-locations" : "events";
+                int partition = blockId % 5;
+
+                // Send message to the topic and partition
+                producer.send(new ProducerRecord<>(topic, partition, null, log));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- stream-processing/DataProducer/src/main/java/DataProducerRunner.java
+++ stream-processing/DataProducer/src/main/java/DataProducerRunner.java
@@ -1,3 +1,9 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class DataProducerRunner {
 
     public static void main(String[] args) throws Exception {
@@ -9,6 +15,22 @@ public class DataProducerRunner {
             - Implement the sendData method as required in DataProducer
             - Call the sendData method to start sending data
         */
-            
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.40.109:9092,172.31.46.14:9092,172.31.43.170:9092");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.LINGER_MS_CONFIG, "1");
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432");
+
+        KafkaProducer<String,String> producer = new KafkaProducer<>(props);
+        String traceFile = "tracefile";
+        DataProducer dataProducer = new DataProducer(producer,traceFile);
+
+        dataProducer.sendData();
+        producer.close();
+        System.out.println("Streaming done");
     }
 }
