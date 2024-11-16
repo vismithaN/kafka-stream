779a20907641e56537f34487e8c908ebe8b735c5
Vismitha N <vismitha944@gmail.com>
2024-11-16 01:16:45 -0500

    Final changes

--- stream-processing/DataProducer/src/main/java/DataProducerRunner.java
+++ stream-processing/DataProducer/src/main/java/DataProducerRunner.java
@@ -1,3 +1,9 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class DataProducerRunner {
 
     public static void main(String[] args) throws Exception {
@@ -9,6 +15,22 @@ public class DataProducerRunner {
             - Implement the sendData method as required in DataProducer
             - Call the sendData method to start sending data
         */
-            
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "172.31.32.66:9092,172.31.44.79:9092,172.31.32.106:9092");
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.LINGER_MS_CONFIG, "1");
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "33554432");
+
+        KafkaProducer<String,String> producer = new KafkaProducer<>(props);
+        String traceFile = "trace_task2";
+        DataProducer dataProducer = new DataProducer(producer,traceFile);
+
+        dataProducer.sendData();
+        producer.close();
+        System.out.println("Streaming done");
     }
 }
